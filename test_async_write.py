5451515b50fdd6faf54b40188d9477a8e7b5dd0e
Yanbing Wang <yanbing.wang@vanderbilt.edu>
2022-04-25 10:11:40 -0500

    s

--- test_async_write.py
+++ test_async_write.py
@@ -133,30 +133,31 @@ import pymongo
 client = MongoClient(host=['localhost:27017'])
 db=client["trajectories"]
 col=db["test_change_stream"]
-
+col.drop()
+col=db["test_change_stream"]
 col.insert_one({"x":1})
 print(col.count_documents({}))
-
+col=db["test_change_stream"]
 #%%
-try:
-    resume_token = None
-    pipeline = [{'$match': {'operationType': 'insert'}}]
-    with db.collection.watch(pipeline) as stream:
-        for insert_change in stream:
-            print(insert_change)
-            resume_token = stream.resume_token
-except pymongo.errors.PyMongoError:
-    # The ChangeStream encountered an unrecoverable error or the
-    # resume attempt failed to recreate the cursor.
-    if resume_token is None:
-        # There is no usable resume token because there was a
-        # failure during ChangeStream initialization.
-        logging.error('...')
-    else:
-        # Use the interrupted ChangeStream's resume token to create
-        # a new ChangeStream. The new stream will continue from the
-        # last seen insert change without missing any events.
-        with db.collection.watch(
-                pipeline, resume_after=resume_token) as stream:
-            for insert_change in stream:
-                print(insert_change)
+#try:
+resume_token = None
+#    pipeline = [{'$match': {'operationType': 'insert'}}]
+with col.watch() as stream:
+    for insert_change in stream:
+        print(insert_change["_id"])
+        resume_token = stream.resume_token
+#except pymongo.errors.PyMongoError:
+#    # The ChangeStream encountered an unrecoverable error or the
+#    # resume attempt failed to recreate the cursor.
+#    if resume_token is None:
+#        # There is no usable resume token because there was a
+#        # failure during ChangeStream initialization.
+#        logging.error('...')
+#    else:
+#        # Use the interrupted ChangeStream's resume token to create
+#        # a new ChangeStream. The new stream will continue from the
+#        # last seen insert change without missing any events.
+#        with col.watch(
+#                pipeline, resume_after=resume_token) as stream:
+#            for insert_change in stream:
+#                print(insert_change)
