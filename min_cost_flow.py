017fa69bf0ef3f88e2a8d603e8b778c5368a4ee5
Yanbing Wang git <theoffbeatphd@gmail.com>
2022-06-25 14:14:36 -0500

    Update min_cost_flow.py

--- min_cost_flow.py
+++ min_cost_flow.py
@@ -389,9 +389,9 @@ def dummy_stitcher(old_q, new_q):
         
         time.sleep(0.5)
         
-        stitcher_logger.info("old_q size: {}".format(old_q.qsize()))
         with DelayedKeyboardInterrupt():
             new_q.put(x)
+            stitcher_logger.info("old_q size: {}".format(old_q.qsize()))
         
     stitcher_logger.info("Exiting dummy stitcher while loop")
     sys.exit(2)
