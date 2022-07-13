b8d72cbece2df0bfd39b636f78f438998c6254c2
Yanbing Wang git <theoffbeatphd@gmail.com>
2022-07-13 16:47:54 -0500

    Update live_data_feed.py

--- live_data_feed.py
+++ live_data_feed.py
@@ -140,10 +140,10 @@ def live_data_reader(default_param, east_queue, west_queue, t_buffer = 2, min_qu
                 # push to heap
                 safe_query_time = change["fullDocument"]['first_timestamp']-t_buffer
                 # print(type(change["fullDocument"]['last_timestamp']), type(change["fullDocument"]))
-                heapq.heappush(heap,(change["fullDocument"]['last_timestamp'],change['fullDocument']))
+                heapq.heappush(heap,(change["fullDocument"]['last_timestamp'],change["fullDocument"]['first_timestamp'],change['fullDocument']))
                 # check if heap[0] is ready, pop until it's not ready
                 while heap and heap[0][0] < safe_query_time:
-                    _, doc = heapq.heappop(heap)
+                    _, _,doc = heapq.heappop(heap)
                     print("pop: {}, {}".format(doc["first_timestamp"],doc["last_timestamp"]))
                     if len(doc["timestamp"]) > 3: 
                         if doc["direction"] == 1:
