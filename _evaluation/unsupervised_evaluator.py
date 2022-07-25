840da023edad89505b58fe439755766e9bf42aaf
Yanbing Wang git <theoffbeatphd@gmail.com>
2022-07-25 12:22:10 -0500

    Update unsupervised_evaluator.py

--- _evaluation/unsupervised_evaluator.py
+++ _evaluation/unsupervised_evaluator.py
@@ -16,7 +16,7 @@ Compare raw and reconciled (unsupervised)
 - (done)collision 
 - length, width, height distribution
 - density? flow?
-- lane distribution
+- (done)lane distribution
 
 Examine problematic stitching
 - plot a list of fragments
@@ -26,6 +26,9 @@ Statics output write to
 - DB (?)
 - file
 - log.info(extra={})
+
+TODO
+1. make traj_eval faster using MongoDB projection instead of python
 """
 
 from i24_database_api.db_reader import DBReader
@@ -62,7 +65,9 @@ class UnsupervisedEvaluator():
         
         self.res["collection"] = self.collection_name
         self.res["traj_count"] = self.dbr_v.count()
-        self.res["time_count"] = self.dbr_t.count()
+        self.res["timestamp_count"] = self.dbr_t.count()
+        
+        self.lanes = [i*12 for i in range(-1,12)]
             
        
     def __del__(self):
@@ -144,14 +149,23 @@ class UnsupervisedEvaluator():
             dt = np.diff(traj["timestamp"])[:-1]
             try: return max(ddx/(dt**2))
             except: return np.nan
+            
+        def _get_lane_changes(traj):
+            '''
+            count number of times y position is at another lane according to lane marks
+            '''
+            lane_idx = np.digitize(traj["y_position"], self.lanes)
+            lane_change = np.diff(lane_idx)
+            # count-nonzeros
+            return np.count_nonzero(lane_change)
         
 
-        # distributions
+        # distributions - all the functions that return a single value
         # TODO: put all functions in a separate script
         functions = [_get_duration, _get_x_traveled,
                       _get_y_traveled, _get_max_vx, _get_min_vx,
-                      _get_max_vy, _get_min_vy,_get_max_ax,_get_min_ax]
-        # functions = []
+                      _get_max_vy, _get_min_vy,_get_max_ax,_get_min_ax,_get_lane_changes]
+        # functions = [_get_lane_changes]
         
         for fcn in functions:
             traj_cursor = self.dbr_v.collection.find({})
@@ -165,7 +179,7 @@ class UnsupervisedEvaluator():
             self.res[attr_name]["avg"] = np.nanmean(res)
             self.res[attr_name]["stdev"] = np.nanstd(res)
            
-        # get ids
+        # get ids - all the functions that return other information
         functions = [_get_backward_cars]
         for fcn in functions:
             traj_cursor = self.dbr_v.collection.find({})
@@ -183,78 +197,139 @@ class UnsupervisedEvaluator():
     
     def time_evaluate(self, sample_rate=10):
         '''
-        Evaluate based on time-indexed collection
+        Evaluate using time-indexed collection
         sample_rate: (int) select every sample_rate timestamps to evaluate
         '''
+        # matries to convert [x,y,len,wid] to [lefttop_x, lefttop_y, bottomright_x, bottomright_y]
         east_m = np.array([[1, 0,0,0], [0,1,0,0.5], [1,0,1,0], [0,1,0,-0.5]]).T
         west_m = np.array([[1,0,-1,0], [0,1,0,0.5], [1, 0,0,0], [0,1,0,-0.5]]).T
         
         def doOverlap(pts1, pts2):
             '''
             pts: [lefttop_x, lefttop_y, bottomright_x, bottomright_y]
+            return True if two rectangles overlap
             '''
             # by separating axix theorem
             return not (pts1[0] > pts2[2] or pts1[1] < pts2[3] or pts1[2] < pts2[0] or pts1[3] > pts2[1] )
 
-
-        def _get_overlaps(time_doc):
+        def calc_space_gap(pts1, pts2):
+            '''
+            pts: [lefttop_x, lefttop_y, bottomright_x, bottomright_y]
+            if two cars are on the same lane, get the space gap
+            '''
+            if abs(pts1[1] + pts1[3] - pts2[1] - pts2[3])/2 < 6: # if two cars are likely to be on the same lane
+                return max(pts2[0] - pts1[2], pts1[0] - pts2[2])
+            else:
+                return None
             
+        def _get_overlaps(time_doc):
+            '''
+            Calculate pair-wise overlaps and space gap at a given timestamp
+            '''
             veh_ids = time_doc['id']
+            pos = time_doc["position"]
+            try:
+                dims = time_doc["dimensions"]
+                time_doc_dict = {veh_ids[i]: pos[i] + dims[i][:2] for i in range(len(veh_ids))}
+                has_dimension = True
+            except KeyError:
+                time_doc_dict = {veh_ids[i]: pos[i] for i in range(len(veh_ids))}
+                has_dimension = False
+                
+            if has_dimension:
+                pipeline = [
+                    {"$match": {"$and" : [{"_id": {"$in": veh_ids}}, {"direction": {"$eq":1}}]}},
+                    {'$project':{ '_id': 1 } },
+                              ]
+                query = self.dbr_v.collection.aggregate(pipeline)
+                east_ids = [doc["_id"] for doc in query]
+                west_ids = list(set(veh_ids) - set(east_ids))
+                east_b = np.array([time_doc_dict[veh_id] for veh_id in east_ids])
+                west_b = np.array([time_doc_dict[veh_id] for veh_id in west_ids])
+                
+            else:
+                east_pipeline = [
+                    {"$match": {"$and" : [{"_id": {"$in": veh_ids}}, {"direction": {"$eq":1}}]}},
+                    {'$project':{ '_id': 1, 'length':1, 'width': 1 } },
+                              ]
+                east_query = self.dbr_v.collection.aggregate(east_pipeline)
+                east_dict = {doc["_id"]: [doc["length"], doc["width"]] for doc in east_query} # get dimension info
+                west_pipeline = [
+                    {"$match": {"$and" : [{"_id": {"$in": veh_ids}}, {"direction": {"$eq":-1}}]}},
+                    {'$project':{ '_id': 1, 'length':1, 'width': 1 } },
+                              ]
+                west_query = self.dbr_v.collection.aggregate(west_pipeline)
+                west_dict = {doc["_id"]: [doc["length"], doc["width"]] for doc in west_query} # get dimension info
+                
+                east_ids = list(east_dict.keys())
+                west_ids = list(west_dict.keys())
+                east_b = np.array([time_doc_dict[veh_id]+east_dict[veh_id] for veh_id in east_ids])
+                west_b = np.array([time_doc_dict[veh_id]+west_dict[veh_id] for veh_id in west_ids])
+                
+            # print(has_dimension) 
+            # print(east_b.shape)   
             overlap = []
+            space_gap = []
             # veh_cache = {} # key: vehicle_id, val: [lx, ly, rx, ry]
             #a = M*b, where a=[lx, ly, rx, ry], b =[x,y,len,wid]
             # vectorize to all vehicles: A = M*B
-    
-            # get east bound boxes
-            pipeline = [
-                {"$match": {"$and" : [{"_id": {"$in": veh_ids}}, {"direction": {"$eq":1}}]}},
-                {'$project':{ '_id': 1 } },
-                          ]
-            east_ids = self.dbr_v.collection.aggregate(pipeline)
-            east_ids = [doc["_id"] for doc in east_ids]
-            if east_ids:
-                east_b = np.array([time_doc['position'][i]+time_doc['dimensions'][i][:2] for i in range(len(veh_ids)) if veh_ids[i] in east_ids])
+            try:
                 east_pts = np.matmul(east_b, east_m)
-                for i, pts1 in enumerate(east_pts):
-                    for j, pts2 in enumerate(east_pts[i+1:]):
-                        if doOverlap(pts1, pts2):
-                            overlap.append([str(east_ids[i]),str(east_ids[j])])
+            except ValueError: # ids are empty
+                east_pts = []
+
+            for i, pts1 in enumerate(east_pts):
+                for j, pts2 in enumerate(east_pts[i+1:]):
+                    # check if two boxes overlap
+                    if doOverlap(pts1, pts2):
+                        overlap.append((str(east_ids[i]),str(east_ids[j])))
+                    # get space gap
+                    gap = calc_space_gap(pts1, pts2)
+                    if gap: space_gap.append(gap)
 
             # west bound
-            west_ids = list(set(veh_ids) - set(east_ids))
-            if west_ids:
-                west_b = np.array([time_doc['position'][i]+time_doc['dimensions'][i][:2] for i in range(len(veh_ids)) if veh_ids[i] in west_ids])
+            try:
                 west_pts = np.matmul(west_b, west_m)
-                # this can be optimized - not time consuming
-                for i, pts1 in enumerate(west_pts):
-                    for j, pts2 in enumerate(west_pts[i+1:]):
-                        if doOverlap(pts1, pts2):
-                            overlap.append([str(west_ids[i]),str(west_ids[j])])
-            return overlap
-            # return list(east_ids)
+            except ValueError:
+                west_pts = []
+            # this can be optimized - not time consuming
+            for i, pts1 in enumerate(west_pts):
+                for j, pts2 in enumerate(west_pts[i+1:]):
+                    # check if two boxes overlap
+                    if doOverlap(pts1, pts2):
+                        overlap.append((str(west_ids[i]),str(west_ids[j])))
+                    # get space gap
+                    gap = calc_space_gap(pts1, pts2)
+                    if gap: space_gap.append(gap)
+                        
+            return overlap, space_gap
+
                     
         # start thread_pool for each timestamp
         time_cursor = self.dbr_t.collection.find({})
-        overlaps = self.thread_pool(_get_overlaps, iterable=time_cursor)
+        res = self.thread_pool(_get_overlaps, iterable=time_cursor) # [[overlap, spacegap], [overlap, spacegap],...]
         # overlaps = set(overlaps) # get the unique values only - unhashable
-        overlaps = [r for r in overlaps if r] # only report the overlaps
+        # overlaps = [r[0] for r in res if r[0]] # only report the overlaps
+        # print([r[0] for r in res if r[0]])
+        overlaps = set()
+        dummy = [overlaps.add(rr) for r in res for rr in r[0]]
+        space_gaps = [min(r[1]) for r in res if r[1]] 
         
-        self.res["overlaps"] = overlaps
+        print("Evaluating overlaps...")
+        self.res["overlaps"] = list(overlaps)
+        
+        print("Evaluating min_space_gaps...")
+        self.res["min_space_gaps"]["min"] = min(space_gaps)
+        self.res["min_space_gaps"]["max"] = max(space_gaps)
+        self.res["min_space_gaps"]["avg"] = np.mean(space_gaps)
+        self.res["min_space_gaps"]["median"] = np.median(space_gaps)
         
         return
-            
-            
-            
+    
+
             
         
-        def _get_min_spacing(time):
-            # calculate pair-wise spacing of all pairs at this timestamp
-            # pair will be on the same lane (digitize y)
-            # get the min
-            return  
-
-        def _get_car_following_pairs(time):
-            return
+    
         
         
         
@@ -262,8 +337,21 @@ class UnsupervisedEvaluator():
         pprint.pprint(self.res, width = 1)
     
     def save_res(self):
+        class NpEncoder(json.JSONEncoder):
+            def default(self, obj):
+                if isinstance(obj, np.integer):
+                    return int(obj)
+                if isinstance(obj, np.floating):
+                    return float(obj)
+                if isinstance(obj, np.ndarray):
+                    return obj.tolist()
+                return super(NpEncoder, self).default(obj)
+    
+
+
         with open(f"res_{self.collection_name}.json", "w") as f:
-            json.dump(self.res, f, indent=4)
+            json.dump(self.res, f, indent=4, sort_keys=False,cls=NpEncoder)
+        print("saved.")
             
 
 
@@ -428,8 +516,9 @@ if __name__ == '__main__':
     
     trajectory_database = "trajectories"
     timestamp_database = "transformed"
-    collection = "21_07_2022_gt1_alpha"
-    # collection = "tracking_v1"
+    # collection = "21_07_2022_gt1_alpha"
+    # collection = "batch_5_07072022"
+    collection = "groundtruth_scene_1"
 
     ue = UnsupervisedEvaluator(config, trajectory_database=trajectory_database, timestamp_database = timestamp_database,
                                collection_name=collection, num_threads=200)
@@ -439,17 +528,13 @@ if __name__ == '__main__':
     t2 = time.time()
     
     print("time: ", t2-t1)
-    ue.print_res()
+    # ue.print_res()
     ue.save_res()
     
     
-
-    
-    # db_param.db_name = "transformed"
-    # 
     #%%
-    # fragment_list = [ObjectId('62d9bbfcd22eff2a0413727f'), ObjectId('62d9bc3ad22eff2a04137295')]
-    # rec_id = ObjectId("62c730078b650aa00a3b925f")
+    # fragment_list = [ObjectId('62d5a345172006d4926ddae3'), ObjectId('62d5a240172006d4926ddab7')]
+    # # rec_id = ObjectId("62c730078b650aa00a3b925f")
     # ue.plot_fragments(fragment_list)
     
     
