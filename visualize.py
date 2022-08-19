338e190118cb41726567590673c39fb6bd72259d
Yanbing Wang git <theoffbeatphd@gmail.com>
2022-08-19 12:23:33 -0500

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -388,7 +388,7 @@ def main(rec, gt = "groundtruth_scene_1_130", framerate = 25, x_min=0, x_max=200
     
 if __name__=="__main__":
 
-    main(rec = "sanctimonious_beluga--RAW_GT1__administers", save=False, offset=70)
+    main(rec = "sanctimonious_beluga--RAW_GT1__administers", save=True)
 
 
     
